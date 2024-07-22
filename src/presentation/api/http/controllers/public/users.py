9d403fdcac0e818080c5713471cba28c8b9dfae0
firminoneto11 <firminoneto11@hotmail.com>
2024-07-22 20:05:37 -0300

    chore: using decorators instead

--- src/presentation/api/http/controllers/public/users.py
+++ src/presentation/api/http/controllers/public/users.py
@@ -3,70 +3,70 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/users")
 
 
-func = lambda: None  # noqa
+@router.post("", summary="Creates a new user")
+async def create_user(): ...
 
 
-router.add_api_route(
-    path="",
-    endpoint=func,
-    methods=["POST"],
-    summary="Creates a new user",
+@router.delete(
+    "", summary="Deletes the logged user and notifies other services about this action"
 )
+async def delete_user(): ...
 
-router.add_api_route(
-    path="",
-    endpoint=func,
-    methods=["POST"],
-    summary="Creates a new user",
-)
 
-router.post("", summary="Creates a new user")
-router.post("/login", summary="Generates a new JWT pair")
-router.get(
+@router.post("/login", summary="Generates a new JWT pair")
+async def login(): ...
+
+
+@router.post("/logout", summary="Invalidates all of the JWT pairs of the logged user")
+async def logout(): ...
+
+
+@router.get(
     "/confirm-email/{token}",
     summary="Receives the confirmation token and marks the email as valid",
 )
-router.post(
+async def confirm_email(token: str): ...
+
+
+@router.post(
     "/forgot-password",
     summary="Sends an email with instructions to change the password",
 )
-router.post(
-    "/forgot-password/{token}",
+async def forgot_password(): ...
+
+
+@router.post(
+    "/reset-password/{token}",
     summary=(
-        "Receives the new password together with the confirmation token and invalidates"
-        " all of the previous JWT pairs based on the current time"
+        "Receives the new password together with the confirmation token and invalidates "
+        "all of the previous JWT pairs"
     ),
 )
+async def reset_password(token: str): ...
 
 
-router.post(
-    "/logout",
-    summary=(
-        "Invalidates all of the JWT pairs of the current user based on the current time"
-    ),
-)
-
-router.post(
+@router.post(
     "/refresh-access-token",
-    summary="Generates a new JWT access token for the current user",
+    summary="Generates a new JWT access token for the logged user",
 )
-router.post(
+async def refresh_access_token(): ...
+
+
+@router.post(
     "/refresh-pair",
     summary=(
-        "Generates a new JWT pair for the current user and invalidates all of the "
-        "previous pairs based on the current time"
+        "Generates a new JWT pair for the logged user and invalidates all of the "
+        "previous pairs"
     ),
 )
+async def refresh_pair(): ...
 
 
-router.post(
+@router.post(
     "/change-password",
     summary=(
-        "Changes the current user's password and invalidates all of the current "
-        "valid JWT pairs based on the current time"
+        "Changes the logged user's password and invalidates all of the current valid "
+        "JWT pairs"
     ),
 )
-
-router.delete(
-    "", summary="Deletes the current user and notifies other services about this action"
-)
+async def change_password(): ...
